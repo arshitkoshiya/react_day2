fe294145035a98ae2374d125edc66791fde80358
AK <arshit.n.p.8@gmail.com>
2022-01-19 15:36:20 +0530

    add react code

--- src/Json/index.js
+++ src/Json/index.js
@@ -1,26 +1,91 @@
 import './index.css';
+import Ak from './index.json'
+import React, { Component } from "react";
 
-const ak = [{
-    "name": "hello",
-    "Email": "hello@gmail.com",
-    "Password": "123"
-},
-{
-    "name": "world",
-    "Email": "world@gmail.com",
-    "Password": "123"
-},
-{
-    "name": "ak",
-    "Email": "ak@gmail.com",
-    "Password": "123"
-},
-{
-    "name": "arshit",   
-    "Email": "arshit@gmail.com",
-    "Password": "123"
+class login extends Component {
+    constructor(props){
+        super(props);
+        this.state = { 
+            email:'',
+            password:''
+
+        };
+    }
+    validation = (event) => {
+		var email = document.getElementById("email").value;
+		var password = document.getElementById("password").value;
+		const cEmail = Ak.filter(
+			(validationEmail) => validationEmail.Email == email
+		);
+		if (cEmail.length) {
+			const cPassword = Ak.filter(
+				(ValidationPassword) => ValidationPassword.Password == password
+			);
+			if (cPassword.length) {
+				this.setState({
+					status: "",
+					name: cPassword.map((name) => name.Name),
+					showComponent: true,
+				});
+				event.preventDefault();
+			} else {
+				this.setState({
+					status: "Incorrect Password!!!",
+				});
+			}
+			event.preventDefault();
+		} else {
+			this.setState({
+				status: "User Not Found!!!",
+			});
+			event.preventDefault();
+		}
+	};
+    render() { 
+        return (
+        <div id="form">
+            <form action="#" method="POST">
+
+                <div className="heading">
+                <label>AK GAMMING LOGIN FORM</label>
+                </div>
+
+
+                <div className="fillup">
+                    <table border="0">
+                        <tr>
+                            <td>
+                            <label>E-mail</label>
+                            </td>
+                            <td>
+                            <input type="email" name="email" id="email" placeholder="ak_gamming_08@Email.com " required/>
+                            <div id='wom'>Email is not valid</div>
+                            </td>
+                            <td>
+                            <label>Password</label>
+                            </td>
+                            <td>
+                            <input type="password" name="" id="password" placeholder="enter a strong password" required/>
+                            <div id='wop'>Password is not valid</div>
+                            </td>
+                            <td>
+                            <button type='button'onclick={this.validation}>SUBMIT</button>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+
+            </form>
+            
+        </div>
+        );
+    }
 }
-]
+ 
+export default login;
+
+
+/*
 const validation = () => {
 
     let Email = document.getElementById("email").value;
@@ -33,8 +98,6 @@ const validation = () => {
 
                 if (ak[index]['Password'] === password) {
                     document.getElementById("form").innerHTML = `<h1> ${ak[index]["name"]} </h1>`;
-
-                    
 
                     break;
 
@@ -56,45 +119,6 @@ const validation = () => {
             break;
         }
     }
+};*/
 
 
-};
-export default function Index() {
-     return (
-    <div id="form">
-        <form action="#" method="get">
-
-            <div className="heading">
-                <label>AK GAMMING LOGIN FORM</label>
-            </div>
-
-
-            <div className="fillup">
-                <table border="0">
-                    <tr>
-                        <td>
-                            <label>E-mail</label>
-                        </td>
-                        <td>
-                            <input type="email" name="email" id="email" placeholder="ak_gamming_08@Email.com " required/>
-                            <div id='wom'>Email is not valid</div>
-                        </td>
-                        <td>
-                            <label>Password</label>
-                        </td>
-                        <td>
-                            <input type="password" name="" id="password" placeholder="enter a strong password" required/>
-                            <div id='wop'>Password is not valid</div>
-                        </td>
-                        <td>
-                            <button type='button' onClick = {validation} >SUBMIT</button>
-
-                        </td>
-                    </tr>
-                </table>
-            </div>
-
-        </form>
-    </div>
-    )
-}
