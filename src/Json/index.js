3414c7a0f7116ad81e4529f96359b1c73c7da029
AK <arshit.n.p.8@gmail.com>
2022-01-21 09:43:49 +0530

    done

--- src/Json/index.js
+++ src/Json/index.js
@@ -1,89 +1,103 @@
-import './index.css';
-import Ak from './index.json'
+import "./index.css";
+import Ak from "./index.json";
 import React, { Component } from "react";
 
 class login extends Component {
-    constructor(props){
-        super(props);
-        this.state = { 
-            email:'',
-            password:''
-
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: "",
+      status: ""
+    };
+  }
+  validation = (event) => {
+      
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    const cEmail = Ak.filter(
+      (validationEmail) => validationEmail.Email === email
+    );
+    if (cEmail.length) {
+      const cPassword = Ak.filter(
+        (ValidationPassword) => ValidationPassword.Password === password
+      );
+      if (cPassword.length) {
+        this.setState({
+          status: "Welcome" + cPassword.map((name) => name.Name),
+          name: cPassword.map((name) => name.Name),
+          showComponent: true,
+        });
+        event.preventDefault();
+      } else {
+        this.setState({
+          status: "Incorrect Password!!!",
+        });
+      }
+      event.preventDefault();
+    } else {
+      this.setState({
+        status: "User Not Found!!!",
+      });
+      event.preventDefault();
     }
-    validation = (event) => {
-		var email = document.getElementById("email").value;
-		var password = document.getElementById("password").value;
-		const cEmail = Ak.filter(
-			(validationEmail) => validationEmail.Email == email
-		);
-		if (cEmail.length) {
-			const cPassword = Ak.filter(
-				(ValidationPassword) => ValidationPassword.Password == password
-			);
-			if (cPassword.length) {
-				this.setState({
-					status: "",
-					name: cPassword.map((name) => name.Name),
-					showComponent: true,
-				});
-				event.preventDefault();
-			} else {
-				this.setState({
-					status: "Incorrect Password!!!",
-				});
-			}
-			event.preventDefault();
-		} else {
-			this.setState({
-				status: "User Not Found!!!",
-			});
-			event.preventDefault();
-		}
-	};
-    render() { 
-        return (
-        <div id="form">
-            <form action="#" method="POST">
+  };
+  render() {
+    return (
+      <div id="form">
+        <form action="#" method="POST">
+          <div className="heading">
+            <label>AK GAMMING LOGIN FORM</label>
+          </div>
 
-                <div className="heading">
-                <label>AK GAMMING LOGIN FORM</label>
-                </div>
+          <div className="fillup">
+            <table border="0">
+              <tr>
+                <td>
+                  <label>E-mail</label>
+                </td>
+                <td>
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    placeholder="ak_gamming_08@Email.com "
+                    required
+                  />
+                  <div id="wom">Email is not valid</div>
+                </td>
+                <td>
+                  <label>Password</label>
+                </td>
+                <td>
+                  <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    placeholder="enter a strong password"
+                    required
+                  />
+                  <div id="wop">Password is not valid</div>
+                </td>
+                <td>
+                  <button type="button" onClick={this.validation}>
+                    SUBMIT
+                  </button>
 
-
-                <div className="fillup">
-                    <table border="0">
-                        <tr>
-                            <td>
-                            <label>E-mail</label>
-                            </td>
-                            <td>
-                            <input type="email" name="email" id="email" placeholder="ak_gamming_08@Email.com " required/>
-                            <div id='wom'>Email is not valid</div>
-                            </td>
-                            <td>
-                            <label>Password</label>
-                            </td>
-                            <td>
-                            <input type="password" name="" id="password" placeholder="enter a strong password" required/>
-                            <div id='wop'>Password is not valid</div>
-                            </td>
-                            <td>
-                            <button type='button'onclick={this.validation}>SUBMIT</button>
-                            </td>
-                        </tr>
-                    </table>
-                </div>
-
-            </form>
+                </td>
+              </tr>
+            </table>
+            {this.state.status}
             
-        </div>
-        );
-    }
+            
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
- 
-export default login;
 
+export default login;
 
 /*
 const validation = () => {
@@ -120,5 +134,3 @@ const validation = () => {
         }
     }
 };*/
-
-
